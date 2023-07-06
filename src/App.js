c24bcc176a75467af29327c9742ce490a7e923b4
gabrielmussafiri <musgabriel2@gmail.com>
2023-07-06 16:44:52 +0200

    clear items functions updated

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ function App() {
   }
 
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm("Are you sure you want to clear the list");
+    if (confirmed) setItems([]);
   }
 
   return (
